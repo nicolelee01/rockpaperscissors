25b2903809a4046557fb4d1b2e3c821045c7f956
Nicole Lee <nicole01@stanford.edu>
2023-02-16 19:51:22 -0800

    prints if it's a win, tie, or loss

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,7 +8,32 @@ def main():
     print("\n")
     print("Type in your selection (rock, paper, or scissors)")
     userSelection = input()
-    computerSelection = random.choice(["rock", "paper", "scissor"])
+    computerSelection = random.choice(["rock", "paper", "scissors"])
+    print("The computer said {}!".format(computerSelection))
+    result = None
+    if userSelection == computerSelection:
+        result = "tie"
+    elif userSelection == "rock":
+        if computerSelection == "paper":
+            result = "lose"
+        else:
+            result = "win"
+    elif userSelection == "paper":
+        if computerSelection == "rock":
+            result = "win"
+        else:
+            result = "lose"
+    else:
+        if computerSelection == "rock":
+            result = "lose"
+        else:
+            result = "win"
+    if result == "tie":
+        print("It was a tie!")
+    elif result == "win":
+        print("You won!")
+    else:
+        print("You lost!")
 
 if __name__ == "__main__":
     main()
