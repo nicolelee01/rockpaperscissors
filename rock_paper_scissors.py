be0d2572025d1e6425c389571ae06a6dc0af604f
Nicole Lee <nicole01@stanford.edu>
2023-02-16 19:53:38 -0800

    added a new function to run the game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,34 +6,37 @@ def main():
     print("Here are the rules of Rock, Paper, Scissors:")
     print("Two players secretly pick one of 'rock,' 'paper,' or 'scissors.' Both players reveal their selection to the other player at once; the winner is chosen based on what the selections are. Rock beats scissors (by crushing them); scissors beats paper (by cutting it); and paper beats rock (by covering it). If both players select the same one, it is a tie.")
     print("\n")
-    print("Type in your selection (rock, paper, or scissors)")
-    userSelection = input()
-    computerSelection = random.choice(["rock", "paper", "scissors"])
-    print("The computer said {}!".format(computerSelection))
-    result = None
-    if userSelection == computerSelection:
-        result = "tie"
-    elif userSelection == "rock":
-        if computerSelection == "paper":
-            result = "lose"
+
+    def run():
+        print("Type in your selection (rock, paper, or scissors)")
+        userSelection = input()
+        computerSelection = random.choice(["rock", "paper", "scissors"])
+        print("The computer said {}!".format(computerSelection))
+        result = None
+        if userSelection == computerSelection:
+            result = "tie"
+        elif userSelection == "rock":
+            if computerSelection == "paper":
+                result = "lose"
+            else:
+                result = "win"
+        elif userSelection == "paper":
+            if computerSelection == "rock":
+                result = "win"
+            else:
+                result = "lose"
         else:
-            result = "win"
-    elif userSelection == "paper":
-        if computerSelection == "rock":
-            result = "win"
+            if computerSelection == "rock":
+                result = "lose"
+            else:
+                result = "win"
+        if result == "tie":
+            print("It was a tie!")
+        elif result == "win":
+            print("You won!")
         else:
-            result = "lose"
-    else:
-        if computerSelection == "rock":
-            result = "lose"
-        else:
-            result = "win"
-    if result == "tie":
-        print("It was a tie!")
-    elif result == "win":
-        print("You won!")
-    else:
-        print("You lost!")
+            print("You lost!")
+    run()
 
 if __name__ == "__main__":
     main()
