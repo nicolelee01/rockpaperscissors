859d781b3af0993f8e066798c76f1fefa6d41935
Nicole Lee <nicole01@stanford.edu>
2023-02-16 19:40:56 -0800

    added user selection input

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,6 +3,9 @@ def main():
     print("\n")
     print("Here are the rules of Rock, Paper, Scissors:")
     print("Two players secretly pick one of 'rock,' 'paper,' or 'scissors.' Both players reveal their selection to the other player at once; the winner is chosen based on what the selections are. Rock beats scissors (by crushing them); scissors beats paper (by cutting it); and paper beats rock (by covering it). If both players select the same one, it is a tie.")
+    print("\n")
+    print("Type in your selection (rock, paper, or scissors)")
+    userSelection = input()
 
 if __name__ == "__main__":
     main()
