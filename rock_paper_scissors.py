dcd31dc04237b5f30564454ed498caeb154f1598
Nicole Lee <nicole01@stanford.edu>
2023-02-16 18:00:31 -0800

    added rules

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,3 @@
 print("Hello user! Let's play a game of Rock, Paper, Scissors!")
+print("Here are the rules of Rock, Paper, Scissors:")
+print("Two players secretly pick one of 'rock,' 'paper,' or 'scissors.' Both players reveal their selection to the other player at once; the winner is chosen based on what the selections are. Rock beats scissors (by crushing them); scissors beats paper (by cutting it); and paper beats rock (by covering it). If both players select the same one, it is a tie.")
