5e7329d71d41e366ad30d82e17c0877100163eb4
Nicole Lee <nicole01@stanford.edu>
2023-02-16 19:38:12 -0800

    main function added

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,9 @@
-print("Hello user! Let's play a game of Rock, Paper, Scissors!")
-print("Here are the rules of Rock, Paper, Scissors:")
-print("Two players secretly pick one of 'rock,' 'paper,' or 'scissors.' Both players reveal their selection to the other player at once; the winner is chosen based on what the selections are. Rock beats scissors (by crushing them); scissors beats paper (by cutting it); and paper beats rock (by covering it). If both players select the same one, it is a tie.")
+def main():
+    print("Hello user! Let's play a game of Rock, Paper, Scissors!")
+    print("\n")
+    print("Here are the rules of Rock, Paper, Scissors:")
+    print("Two players secretly pick one of 'rock,' 'paper,' or 'scissors.' Both players reveal their selection to the other player at once; the winner is chosen based on what the selections are. Rock beats scissors (by crushing them); scissors beats paper (by cutting it); and paper beats rock (by covering it). If both players select the same one, it is a tie.")
+
+if __name__ == "__main__":
+    main()
+
